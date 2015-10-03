c1eff99fb6a6a51f9cf7e5d2428f996dd0ed9583
idhammath <idham.math@gmail.com>
2015-10-03 16:31:21 +0700

    Update onionhead.js

--- onionhead.js
+++ onionhead.js
@@ -21,6 +21,9 @@ $(document).ready(function(){
 		this.innerHTML = text.replace(':terpana', "<img src='http://emoticoner.com/files/emoticons/onion-head/admire-onion-head-emoticon.gif?1292862486'>");
 	});
 	$('.comment-form').find('p').html(function (index,text){
-		this.innerHTML=text.replace('[tempat-emot]',"<div style='text-align:center'><img src='http://emoticoner.com/files/emoticons/onion-head/admire-onion-head-emoticon.gif?1292862486' onclick='fungsiemot(1)'/><input id='emot1' style='width:100px; display:none;' type='text' value=':terkesima'/><img onclick='fungsiemot(2)' src='http://emoticoner.com/files/emoticons/onion-head/admire2-onion-head-emoticon.gif?1292862489'/><input id='emot2' style='width:100px; display:none;' type='text' value=':terpana'/><img onclick='fungsiemot(3)' src='http://emoticoner.com/files/emoticons/onion-head/ahaaah-onion-head-emoticon.gif?1292862489'/><input id='emot3' style='width:50px; display:none;' type='text' value=':aha'/><img onclick='fungsiemot(4)' src='http://emoticoner.com/files/emoticons/onion-head/angel1-onion-head-emoticon.gif?1292862489'/><input id='emot4' style='width:50px; display:none;' type='text' value=':angel'/></div>");
+		this.innerHTML=text.replace('[tempat-emot]',"<div style='text-align:center'><img src='http://emoticoner.com/files/emoticons/onion-head/admire-onion-head-emoticon.gif?1292862486' onclick='fungsiemot(1)'/><input id='emot1' style='width:100px; display:none;' type='text' value=':terkesima'/>
+		<img onclick='fungsiemot(2)' src='http://emoticoner.com/files/emoticons/onion-head/admire2-onion-head-emoticon.gif?1292862489'/><input id='emot2' style='width:100px; display:none;' type='text' value=':terpana'/>
+		<img onclick='fungsiemot(3)' src='http://emoticoner.com/files/emoticons/onion-head/ahaaah-onion-head-emoticon.gif?1292862489'/><input id='emot3' style='width:50px; display:none;' type='text' value=':aha'/>
+		<img onclick='fungsiemot(4)' src='http://emoticoner.com/files/emoticons/onion-head/angel1-onion-head-emoticon.gif?1292862489'/><input id='emot4' style='width:50px; display:none;' type='text' value=':angel'/></div>");
 	});
 });
